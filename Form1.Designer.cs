18a31e530df0390c91aaa610518dde51a9d81d88
Adair Soto <adair.soto@protonmail.com>
2022-05-02 08:51:52 -0300

    1st commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Class1_WF3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCreate = new System.Windows.Forms.Label();
+            this.txtCreate = new System.Windows.Forms.TextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtDelete = new System.Windows.Forms.TextBox();
+            this.lblOrigin = new System.Windows.Forms.Label();
+            this.txtOrigin = new System.Windows.Forms.TextBox();
+            this.lblDestination = new System.Windows.Forms.Label();
+            this.txtDestination = new System.Windows.Forms.TextBox();
+            this.btnMove = new System.Windows.Forms.Button();
+            this.lblDelete = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblCreate
+            // 
+            this.lblCreate.AutoSize = true;
+            this.lblCreate.Location = new System.Drawing.Point(50, 51);
+            this.lblCreate.Name = "lblCreate";
+            this.lblCreate.Size = new System.Drawing.Size(108, 13);
+            this.lblCreate.TabIndex = 0;
+            this.lblCreate.Text = "Folder To Be Created";
+            // 
+            // txtCreate
+            // 
+            this.txtCreate.Location = new System.Drawing.Point(53, 67);
+            this.txtCreate.Name = "txtCreate";
+            this.txtCreate.Size = new System.Drawing.Size(100, 20);
+            this.txtCreate.TabIndex = 1;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(210, 64);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 2;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(210, 122);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtDelete
+            // 
+            this.txtDelete.Location = new System.Drawing.Point(53, 125);
+            this.txtDelete.Name = "txtDelete";
+            this.txtDelete.Size = new System.Drawing.Size(100, 20);
+            this.txtDelete.TabIndex = 3;
+            // 
+            // lblOrigin
+            // 
+            this.lblOrigin.AutoSize = true;
+            this.lblOrigin.Location = new System.Drawing.Point(69, 272);
+            this.lblOrigin.Name = "lblOrigin";
+            this.lblOrigin.Size = new System.Drawing.Size(84, 13);
+            this.lblOrigin.TabIndex = 7;
+            this.lblOrigin.Text = "Origin File Name";
+            // 
+            // txtOrigin
+            // 
+            this.txtOrigin.Location = new System.Drawing.Point(62, 288);
+            this.txtOrigin.Name = "txtOrigin";
+            this.txtOrigin.Size = new System.Drawing.Size(100, 20);
+            this.txtOrigin.TabIndex = 6;
+            // 
+            // lblDestination
+            // 
+            this.lblDestination.AutoSize = true;
+            this.lblDestination.Location = new System.Drawing.Point(57, 329);
+            this.lblDestination.Name = "lblDestination";
+            this.lblDestination.Size = new System.Drawing.Size(110, 13);
+            this.lblDestination.TabIndex = 9;
+            this.lblDestination.Text = "Destination File Name";
+            // 
+            // txtDestination
+            // 
+            this.txtDestination.Location = new System.Drawing.Point(62, 345);
+            this.txtDestination.Name = "txtDestination";
+            this.txtDestination.Size = new System.Drawing.Size(100, 20);
+            this.txtDestination.TabIndex = 8;
+            // 
+            // btnMove
+            // 
+            this.btnMove.Location = new System.Drawing.Point(210, 288);
+            this.btnMove.Name = "btnMove";
+            this.btnMove.Size = new System.Drawing.Size(75, 23);
+            this.btnMove.TabIndex = 10;
+            this.btnMove.Text = "Move";
+            this.btnMove.UseVisualStyleBackColor = true;
+            this.btnMove.Click += new System.EventHandler(this.btnMove_Click);
+            // 
+            // lblDelete
+            // 
+            this.lblDelete.AutoSize = true;
+            this.lblDelete.Location = new System.Drawing.Point(50, 109);
+            this.lblDelete.Name = "lblDelete";
+            this.lblDelete.Size = new System.Drawing.Size(108, 13);
+            this.lblDelete.TabIndex = 11;
+            this.lblDelete.Text = "Folder To Be Deleted";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(80, 227);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(54, 13);
+            this.lblName.TabIndex = 13;
+            this.lblName.Text = "File Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(62, 243);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblDelete);
+            this.Controls.Add(this.btnMove);
+            this.Controls.Add(this.lblDestination);
+            this.Controls.Add(this.txtDestination);
+            this.Controls.Add(this.lblOrigin);
+            this.Controls.Add(this.txtOrigin);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.txtDelete);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.txtCreate);
+            this.Controls.Add(this.lblCreate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCreate;
+        private System.Windows.Forms.TextBox txtCreate;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtDelete;
+        private System.Windows.Forms.Label lblOrigin;
+        private System.Windows.Forms.TextBox txtOrigin;
+        private System.Windows.Forms.Label lblDestination;
+        private System.Windows.Forms.TextBox txtDestination;
+        private System.Windows.Forms.Button btnMove;
+        private System.Windows.Forms.Label lblDelete;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Class1_WF3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            string folder = $@"c:\{txtCreate.Text}";
+            if (!System.IO.Directory.Exists(folder))
+            {
+                System.IO.Directory.CreateDirectory(folder);
+                txtCreate.Clear();
+            }
+                  
+            else 
+                MessageBox.Show("Folder Exists Already", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtCreate.Clear();
+        }
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string folder = $@"c:\{txtDelete.Text}";
+            if (System.IO.Directory.Exists(folder))
+            {
+                System.IO.Directory.Delete(folder);
+                txtDelete.Clear();
+            }                
+            else
+                MessageBox.Show("Folder Not Found", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtDelete.Clear();
+        }
+        private void btnMove_Click(object sender, EventArgs e)
+        {
+            string fileName = txtName.Text;
+            string originFolder = $@"c:\{txtOrigin.Text}";
+            string destinationFolder = $@"c:\{txtDestination.Text}";
+
+            string originFile = System.IO.Path.Combine(originFolder, fileName);
+            string destinationFile = System.IO.Path.Combine(destinationFolder, fileName);
+
+            System.IO.File.Move(originFile, destinationFile);
+        }
+    }
+}
